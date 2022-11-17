55c848dc869a6f87b78374334058b317f21255b5
victorien <deadvicmangame@gmail.com>
2022-11-17 15:57:23 +0100

    exo 52 les conditions js

--- exercice 1/exercice1.js
+++ exercice 1/exercice1.js
@@ -1,6 +1,15 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
+document.getElementById('monDiv').innerHTML = "La variable x est supérieure à la variable y";
+let x = 9;
+let y = 7;
 
-if x > y
-    document.getElementById('monDiv').innerHTML = "La variable x est supérieure à la variable y";
+if (x > y ) {
+    console.log("x est superieur a y ")
+}
+
+
+
+
+
 
 
--- exercice2/exercice2.js
+++ exercice2/exercice2.js
@@ -1,9 +1,12 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
-
-
-(x  y) {
+let x = 1;
+let y = 0;
+if (x > y) {
+    console.log(x > y)//true
+    alert("x est superieur a y")
     document.getElementById('monDiv').innerHTML = "La variable x est supérieure à la variable y";
-}  {
+} else {
+    alert("x n'est pas inferieur a y")//false
     document.getElementById('monDiv').innerHTML = "La variable x n'est pas supérieure à la variable y";
 }
 
